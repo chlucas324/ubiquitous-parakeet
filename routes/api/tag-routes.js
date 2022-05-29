29e2eaecc784ea7993dfe33a96b758ffac983d0f
chlucas324 <chlucas324@gmail.com>
2022-05-29 17:12:52 -0400

    delete const from tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { eq } = require('sequelize/types/lib/operators');
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
